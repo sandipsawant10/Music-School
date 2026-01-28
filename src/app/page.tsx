e87c94b156ed87e20c654c7ce58a479b3645bdd4
sandipsawant10 <sandipsawant503@gmail.com>
2026-01-28 19:21:56 +0530

    Add new components and animations; update Tailwind configuration and dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import { main } from "framer-motion/client";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-origin-white/[0.02]">
-      <h1 className="text-2xl text-center">Chai aur Code</h1>
+     
+      <HeroSection />
+      <FeaturedCourses/>
+      <WhyChooseUs/>
     </main>
   );
 }
